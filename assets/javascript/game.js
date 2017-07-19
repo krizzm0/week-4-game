a8c7c4690d9144b55f4ed636fbefb4b1dfa03de4
kris mo <kh.mo19@gmail.com>
2017-07-19 12:46:26 -0500

    finished attack button, message display, hp counter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,99 +1,159 @@
 
-var playerCharacter;
-var enemyCharacter;
+var player = {
+	name: undefined,
+	hp: 0,
+	attackPWR: 0,
+	attackINCR: 0,
+	counterPWR: 0,
+	html: undefined
+};
+
+var enemy = {
+	name: undefined,
+	hp: 0,
+	attackPWR: 0,
+	attackINCR: 0,
+	counterPWR: 0,
+	html: undefined
+};
+
 var enemyChosen = false;
-var yoda;
-var vadar;
-var luke;
-var darth;
+var yodaHTML;
+var vadarHTML;
+var lukeHTML;
+var darthHTML;
 
 // When HTML is completely loaded, run the follow JavaScript code:
 $(document).ready(function() {
 
 	$("#yoda").on("click", function() {
-		if (playerCharacter === undefined) {
+		if (player.name === undefined) {
 			$("#yoda").removeClass("hoverGlowGreen");
-			yoda = $("#yoda")[0].outerHTML;
-			playerCharacter = yoda;
+			$("#yoda .HP").addClass("userHP");
+			yodaHTML = $("#yoda")[0].outerHTML;
+			player.html = yodaHTML;
+			player.name = "Master Yoda";
+			player.hp = 120;
+			player.attackPWR = 75;
+			player.attackINCR = 10;
 			$("#yoda").addClass("chosenUserPick").removeClass("notChosen");
 			$(".notChosen").removeClass("hoverGlowGreen").addClass("hoverGlowRed");
-			$(".fightSection").append(playerCharacter);
+			$(".fightSection").append(player.html);
 			$("h2").first().removeClass("greenTextGlow").addClass("redTextGlow").text("Select Your Enemy");
-		} else if (playerCharacter !== yoda && enemyChosen === false) {
+		} else if (player.name !== "Master Yoda" && enemyChosen === false) {
 			enemyChosen = true;
 			$("#yoda").removeClass("hoverGlowRed");
-			yoda = $("#yoda")[0].outerHTML;
-			enemyCharacter = yoda;
+			$("#yoda .HP").addClass("enemyHP");
+			yodaHTML = $("#yoda")[0].outerHTML;
+			enemy.html = yodaHTML;
+			enemy.name = "Master Yoda";
+			enemy.hp = 120;
+			enemy.counterPWR = 50;
 			$("#initialHTML").hide();
 			$("#fightHTML").removeClass("invisible").addClass("visible");
 			$(".fightSection").append("<img src='assets/images/attack.png' id='attackSaber'>");
-			$(".fightSection").append(enemyCharacter);
+			$(".fightSection").append(enemy.html);
 		}
 	});
 
 	$("#vadar").on("click", function() {
-		if (playerCharacter === undefined) {
+		if (player.name === undefined) {
 			$("#vadar").removeClass("hoverGlowGreen");
-			vadar = $("#vadar")[0].outerHTML;
-			playerCharacter = vadar;
-			$("#vadar").addClass("chosenUserPick").removeClass("notChosen");
+			$("#vadar .HP").addClass("userHP");
+			vadarHTML = $("#vadar")[0].outerHTML;
+			player.html = vadarHTML;
+			player.name = "Darth Vadar";
+			player.hp = 180;
+			player.attackPWR = 50;
+			player.attackINCR = 20;
+			$("#vadar").addClass("chosenUserPick userHP").removeClass("notChosen");
 			$(".notChosen").removeClass("hoverGlowGreen").addClass("hoverGlowRed");
-			$(".fightSection").append(playerCharacter);
+			$(".fightSection").append(player.html);
 			$("h2").first().removeClass("greenTextGlow").addClass("redTextGlow").text("Select Your Enemy");
-		} else if (playerCharacter !== vadar && enemyChosen === false) {
+		} else if (player.name !== "Darth Vadar" && enemyChosen === false) {
 			enemyChosen = true;
 			$("#vadar").removeClass("hoverGlowRed");
-			vadar = $("#vadar")[0].outerHTML;
-			enemyCharacter = vadar;
+			$("#vadar .HP").addClass("enemyHP");
+			vadarHTML = $("#vadar")[0].outerHTML;
+			enemy.html = vadarHTML;
+			enemy.name = "Darth Vadar";
+			enemy.hp = 180;
+			enemy.counterPWR = 60;
 			$("#initialHTML").hide();
 			$("#fightHTML").removeClass("invisible").addClass("visible");
 			$(".fightSection").append("<img src='assets/images/attack.png' id='attackSaber'>");
-			$(".fightSection").append(enemyCharacter);
+			$(".fightSection").append(enemy.html);
 		}
 	});
 
 	$("#luke").on("click", function() {
-		if (playerCharacter === undefined) {
+		if (player.name === undefined) {
 			$("#luke").removeClass("hoverGlowGreen");
-			luke = $("#luke")[0].outerHTML;
-			playerCharacter = luke;
-			$("#luke").addClass("chosenUserPick").removeClass("notChosen");
+			$("#luke .HP").addClass("userHP");
+			lukeHTML = $("#luke")[0].outerHTML;
+			player.html = lukeHTML;
+			player.name = "Luke Skywalker";
+			player.hp = 150;
+			player.attackPWR = 0;
+			player.attackINCR = 0;
+			$("#luke").addClass("chosenUserPick userHP").removeClass("notChosen");
 			$(".notChosen").removeClass("hoverGlowGreen").addClass("hoverGlowRed");
-			$(".fightSection").append(playerCharacter);
+			$(".fightSection").append(player.html);
 			$("h2").first().removeClass("greenTextGlow").addClass("redTextGlow").text("Select Your Enemy");
-		} else if (playerCharacter !== luke && enemyChosen === false) {
+		} else if (player.name !== "Luke Skywalker" && enemyChosen === false) {
 			enemyChosen = true;
 			$("#luke").removeClass("hoverGlowRed");
-			luke = $("#luke")[0].outerHTML;
-			enemyCharacter = luke;
+			$("#luke .HP").addClass("enemyHP");
+			lukeHTML = $("#luke")[0].outerHTML;
+			enemy.html = lukeHTML;
+			enemy.name = "Luke Skywalker";
+			enemy.hp = 150;
+			enemy.counterPWR = 0;
 			$("#initialHTML").hide();
 			$("#fightHTML").removeClass("invisible").addClass("visible");
 			$(".fightSection").append("<img src='assets/images/attack.png' id='attackSaber'>");
-			$(".fightSection").append(enemyCharacter);
+			$(".fightSection").append(enemy.html);
 		}
 	});
 
 	$("#darth").on("click", function() {
-		if (playerCharacter === undefined) {
+		if (player.name === undefined) {
 			$("#darth").removeClass("hoverGlowGreen");
-			darth = $("#darth")[0].outerHTML;
-			playerCharacter = darth;
-			$("#darth").addClass("chosenUserPick").removeClass("notChosen");
+			$("#darth .HP").addClass("userHP");
+			darthHTML = $("#darth")[0].outerHTML;
+			player.html = darthHTML;
+			player.name = "Darth Maul";
+			player.hp = 130;
+			player.attackPWR = 0;
+			player.attackINCR = 0;
+			$("#darth").addClass("chosenUserPick userHP").removeClass("notChosen");
 			$(".notChosen").removeClass("hoverGlowGreen").addClass("hoverGlowRed");
-			$(".fightSection").append(playerCharacter);
+			$(".fightSection").append(player.html);
 			$("h2").first().removeClass("greenTextGlow").addClass("redTextGlow").text("Select Your Enemy");
-		} else if (playerCharacter !== darth && enemyChosen === false) {
+		} else if (player.name !== "Darth Maul" && enemyChosen === false) {
 			enemyChosen = true;
 			$("#darth").removeClass("hoverGlowRed");
-			darth = $("#darth")[0].outerHTML;
-			enemyCharacter = darth;
+			$("#darth .HP").addClass("enemyHP");
+			darthHTML = $("#darth")[0].outerHTML;
+			enemy.html = darthHTML;
+			enemy.name = "Darth Maul";
+			enemy.hp = 130;
+			enemy.counterPWR = 0;
 			$("#initialHTML").hide();
 			$("#fightHTML").removeClass("invisible").addClass("visible");
 			$(".fightSection").append("<img src='assets/images/attack.png' id='attackSaber'>");
-			$(".fightSection").append(enemyCharacter);
+			$(".fightSection").append(enemy.html);
 		}
 	});
 
-
+$("button").on("click", function() {
+	$("#messageBox").html("You attacked " + enemy.name + " with " + player.attackPWR + " damage<br>");
+	$("#messageBox").append(enemy.name + " counters with " + enemy.counterPWR + " damage");
+	enemy.hp = enemy.hp - player.attackPWR;
+	player.hp = player.hp - enemy.counterPWR;
+	player.attackPWR = player.attackPWR + player.attackINCR;
+	$(".userHP").html(player.hp + "HP");
+	$(".enemyHP").html(enemy.hp + "HP");
+});
 
 }); // End document.ready
