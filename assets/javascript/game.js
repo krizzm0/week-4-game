0370d0dede99a6bfd19144adeb6dda83ce2b8bf0
kris mo <kh.mo19@gmail.com>
2017-07-21 16:27:31 -0500

    game is pretty much finished. added hp health color guide. need to add sound effects next

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
-var gameWon = false;
-
+// Player Object
 var player = {
 	name: undefined,
+	totalHP: 0,
 	hp: 0,
 	attackPWR: 0,
 	attackINCR: 0,
@@ -11,8 +11,10 @@ var player = {
 	killCount: 0
 };
 
+// Enemy Object
 var enemy = {
 	name: undefined,
+	totalHP: 0,
 	hp: 0,
 	attackPWR: 0,
 	attackINCR: 0,
@@ -21,55 +23,96 @@ var enemy = {
 	isDead: false
 };
 
+// Character Object
 var yoda = {
 	isDead: false
 };
-
 var vadar = {
 	isDead: false
 };
-
 var luke = {
 	isDead: false
 };
-
 var darth = {
 	isDead: false
 };
 
-var yodaHTML;
-var vadarHTML;
-var lukeHTML;
-var darthHTML;
+var gameWon = false;
+
+// Functions
+
+// Keeps Track of Enemies Killed and Total Kill Count to Know When to End Game
+function enemyKilled() {
+	player.killCount += 1;
+	if (enemy.name === "Master Yoda") {
+		yoda.isDead = true;
+		// Adds death icon and low opacity to character img
+		$("#yoda img:nth-child(2)").removeClass("invisible");
+		$("#yoda img:nth-child(1)").addClass("dead");
+	} else if (enemy.name === "Darth Vadar") {
+		vadar.isDead = true;
+		$("#vadar img:nth-child(2)").removeClass("invisible");
+		$("#vadar img:nth-child(1)").addClass("dead");
+	} else if (enemy.name === "Luke Skywalker") {
+		luke.isDead = true;
+		$("#luke img:nth-child(2)").removeClass("invisible");
+		$("#luke img:nth-child(1)").addClass("dead");
+	} else if (enemy.name === "Darth Maul") {
+		darth.isDead = true;
+		$("#darth img:nth-child(2)").removeClass("invisible");
+		$("#darth img:nth-child(1)").addClass("dead");
+	}
+
+	if (player.killCount === 3) {
+		gameWon = true;
+	}
+}
+
+// When Game is Lost, Reset Button Shows Up and Reloads Page If Clicked
+function gameOver() {
+	$("#attackBTN").hide();
+	$("#resetBTN").show().on("click", function() {
+		location.reload();
+	});
+}
 
 // When HTML is completely loaded, run the follow JavaScript code:
 $(document).ready(function() {
-
+	// When Yoda is clicked on...
 	$("#yoda").on("click", function() {
+		// If User wants to be Yoda...
 		if (player.name === undefined) {
+			// Removes mouse hover effect and notChosen class (see line 86)
 			$("#yoda").removeClass("hoverGlowGreen notChosen");
+			// Adds userHP class to control HP display
 			$("#yoda .HP").addClass("userHP");
-			yodaHTML = $("#yoda")[0].outerHTML;
-			player.html = yodaHTML;
+			player.html = $("#yoda")[0].outerHTML;
 			player.name = "Master Yoda";
+			player.totalHP = 120;
 			player.hp = 120;
 			player.attackPWR = 8;
 			player.attackINCR = 8;
+			// This class adds a permanent green glow to character
 			$("#yoda").addClass("chosenUserPick");
+			// Gives all other characters red mouse hover effect
 			$(".notChosen").removeClass("hoverGlowGreen").addClass("hoverGlowRed");
+			// Send to fighting stage
 			$(".fightSection").append(player.html);
 			$("h2").first().removeClass("greenTextGlow").addClass("redTextGlow").text("Select Your Enemy");
+		// If User wants Yoda as enemy...
 		} else if (player.name !== "Master Yoda" && yoda.isDead === false) {
+			// Remove hover effect now that Yoda is already selected
 			$("#yoda").removeClass("hoverGlowRed");
+			// Adds enemyHP class to control HP display
 			$("#yoda .HP").addClass("enemyHP");
-			yodaHTML = $("#yoda")[0].outerHTML;
-			enemy.html = yodaHTML;
+			enemy.html = $("#yoda")[0].outerHTML;
 			enemy.name = "Master Yoda";
+			enemy.totalHP = 120;
 			enemy.hp = 120;
 			enemy.counterPWR = 40;
 			enemy.isDead = false;
+			// Go to fighting stage
 			$("#initialHTML").hide();
-			// $("#fightHTML").removeClass("invisible").addClass("visible");
 			$("#fightHTML").show();
 			$(".fightSection").append("<img src='assets/images/attack.png' id='attackSaber'>");
 			$(".fightSection").append(enemy.html);
@@ -80,27 +123,26 @@ $(document).ready(function() {
 		if (player.name === undefined) {
 			$("#vadar").removeClass("hoverGlowGreen notChosen");
 			$("#vadar .HP").addClass("userHP");
-			vadarHTML = $("#vadar")[0].outerHTML;
-			player.html = vadarHTML;
+			player.html = $("#vadar")[0].outerHTML;
 			player.name = "Darth Vadar";
+			player.totalHP = 150;
 			player.hp = 150;
 			player.attackPWR = 6;
-			player.attackINCR = 6;
-			$("#vadar").addClass("chosenUserPick");
+			player.attackINCR = 6
+;			$("#vadar").addClass("chosenUserPick");
 			$(".notChosen").removeClass("hoverGlowGreen").addClass("hoverGlowRed");
 			$(".fightSection").append(player.html);
 			$("h2").first().removeClass("greenTextGlow").addClass("redTextGlow").text("Select Your Enemy");
 		} else if (player.name !== "Darth Vadar" && vadar.isDead === false) {
 			$("#vadar").removeClass("hoverGlowRed");
 			$("#vadar .HP").addClass("enemyHP");
-			vadarHTML = $("#vadar")[0].outerHTML;
-			enemy.html = vadarHTML;
+			enemy.html = $("#vadar")[0].outerHTML;
 			enemy.name = "Darth Vadar";
+			enemy.totalHP = 190;
 			enemy.hp = 190;
 			enemy.counterPWR = 20;
 			enemy.isDead = false;
 			$("#initialHTML").hide();
-			// $("#fightHTML").removeClass("invisible").addClass("visible");
 			$("#fightHTML").show();
 			$(".fightSection").append("<img src='assets/images/attack.png' id='attackSaber'>");
 			$(".fightSection").append(enemy.html);
@@ -111,9 +153,9 @@ $(document).ready(function() {
 		if (player.name === undefined) {
 			$("#luke").removeClass("hoverGlowGreen notChosen");
 			$("#luke .HP").addClass("userHP");
-			lukeHTML = $("#luke")[0].outerHTML;
-			player.html = lukeHTML;
+			player.html = $("#luke")[0].outerHTML;
 			player.name = "Luke Skywalker";
+			player.totalHP = 180;
 			player.hp = 180;
 			player.attackPWR = 11;
 			player.attackINCR = 11;
@@ -124,14 +166,13 @@ $(document).ready(function() {
 		} else if (player.name !== "Luke Skywalker" && luke.isDead === false) {
 			$("#luke").removeClass("hoverGlowRed");
 			$("#luke .HP").addClass("enemyHP");
-			lukeHTML = $("#luke")[0].outerHTML;
-			enemy.html = lukeHTML;
+			enemy.html = $("#luke")[0].outerHTML;
 			enemy.name = "Luke Skywalker";
+			enemy.totalHP = 180;
 			enemy.hp = 180;
 			enemy.counterPWR = 5;
 			enemy.isDead = false;
 			$("#initialHTML").hide();
-			// $("#fightHTML").removeClass("invisible").addClass("visible");
 			$("#fightHTML").show();
 			$(".fightSection").append("<img src='assets/images/attack.png' id='attackSaber'>");
 			$(".fightSection").append(enemy.html);
@@ -142,9 +183,9 @@ $(document).ready(function() {
 		if (player.name === undefined) {
 			$("#darth").removeClass("hoverGlowGreen notChosen");
 			$("#darth .HP").addClass("userHP");
-			darthHTML = $("#darth")[0].outerHTML;
-			player.html = darthHTML;
+			player.html = $("#darth")[0].outerHTML;
 			player.name = "Darth Maul";
+			player.totalHP = 180;
 			player.hp = 180;
 			player.attackPWR = 4;
 			player.attackINCR = 4;
@@ -155,58 +196,81 @@ $(document).ready(function() {
 		} else if (player.name !== "Darth Maul" && darth.isDead === false) {
 			$("#darth").removeClass("hoverGlowRed");
 			$("#darth .HP").addClass("enemyHP");
-			darthHTML = $("#darth")[0].outerHTML;
-			enemy.html = darthHTML;
+			enemy.html = $("#darth")[0].outerHTML;
 			enemy.name = "Darth Maul";
+			enemy.totalHP = 180;
 			enemy.hp = 180;
 			enemy.counterPWR = 25;
 			enemy.isDead = false;
 			$("#initialHTML").hide();
-			// $("#fightHTML").removeClass("invisible").addClass("visible");
 			$("#fightHTML").show();
 			$(".fightSection").append("<img src='assets/images/attack.png' id='attackSaber'>");
 			$(".fightSection").append(enemy.html);
 		}
 	});
 
+	// When the attack button is clicked...
 	$("#attackBTN").on("click", function() {
+		// If enemy isn't killed...
 		if (enemy.isDead === false) {
+			// Attack enemy and display user attack message
 			$("#messageBox").html("You attacked " + enemy.name + " with " + player.attackPWR + " damage<br>");
 			enemy.hp = enemy.hp - player.attackPWR;
 		}
 
+		// But if the enemy is killed from above attack...
 		if (enemy.hp <= 0) {
+			// Show enemy HP as 0 and display defeat message
+			$(".enemyHP").css("color", "red");
 			$(".enemyHP").html("0HP");
 			$("#messageBox").append("You have defeated " + enemy.name + "!");
 			enemy.isDead = true;
+
+		// Else update enemy's new HP with corresponding health colors
 		} else {
+			if ( (enemy.hp > (0.33 * enemy.totalHP)) && (enemy.hp <= (0.66 * enemy.totalHP)) ) {
+				$(".enemyHP").css("color", "orange");
+			} else if ( (enemy.hp <= (0.33 * enemy.totalHP)) ) {
+				$(".enemyHP").css("color", "red");
+			}
 			$(".enemyHP").html(enemy.hp + "HP");
 		}
 
+		// Enemy's turn to attack...
 		if (enemy.isDead === false) {
-
 			$("#messageBox").append(enemy.name + " counters with " + enemy.counterPWR + " damage");
 			player.hp = player.hp - enemy.counterPWR;
+			// If player is killed, game over
 			if (player.hp <= 0) {
+				$(".userHP").css("color", "red");
 				$(".userHP").html("0HP");
 				$("#messageBox").append("<br>" + enemy.name + " has defeated you!");
 				gameOver();
+			// If player is not killed, update player HP
 			} else {
+				if ( (player.hp > (0.33 * player.totalHP)) && (player.hp <= (0.66 * player.totalHP)) ) {
+				$(".userHP").css("color", "orange");
+				} else if ( (player.hp <= (0.33 * player.totalHP)) ) {
+				$(".userHP").css("color", "red");
+				}
 				$(".userHP").html(player.hp + "HP");
 			}
 		}
 
+		// Increase player's attack power
 		player.attackPWR = player.attackPWR + player.attackINCR;
 		
+		// Next button displayed if enemy killed
 		if (enemy.isDead === true) {
-		$("#attackBTN").hide();
-		$("#nextBTN").show();
+			$("#attackBTN").hide();
+			$("#nextBTN").show();
 		}
 	});
 
+	// Next button functionality
 	$("#nextBTN").on("click", function() {
 		enemyKilled();
-		// $("#fightHTML").removeClass("visible").addClass("invisible");
+		// If the game is won...
 		if (gameWon === true) {
 			$("#fightHTML").hide();
 			$("#initialHTML h2").removeClass("redTextGlow").addClass("yellowTextGlow");
@@ -220,6 +284,7 @@ $(document).ready(function() {
 			});
 			$(".character .HP").remove();
 			$("#initialHTML").show();
+		// Else go back to character selection screen to pick new enemy
 		} else {
 			$("#fightHTML").hide();
 			$("#messageBox").html(" ");
@@ -230,35 +295,4 @@ $(document).ready(function() {
 			$("#attackBTN").show();
 		}
 	});
-
-
 }); // End document.ready
-
-
-function enemyKilled() {
-	player.killCount += 1;
-	if (enemy.name === "Master Yoda") {
-		yoda.isDead = true;
-	} else if (enemy.name === "Darth Vadar") {
-		vadar.isDead = true;
-	} else if (enemy.name === "Luke Skywalker") {
-		luke.isDead = true;
-	} else if (enemy.name === "Darth Maul") {
-		darth.isDead = true;
-	}
-
-	if (player.killCount === 3) {
-		gameWon = true;
-	}
-}
-
-function gameOver() {
-	$("#attackBTN").hide();
-	$("#resetBTN").show().on("click", function() {
-		location.reload();
-	});
-}
-
-function gameWin() {
-
-}
